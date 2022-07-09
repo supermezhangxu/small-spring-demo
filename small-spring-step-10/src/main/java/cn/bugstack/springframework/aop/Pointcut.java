8d990dc1099e8c7aa98c0d486af1b77c9acaa09c
zxh <zxh@163.com>
2022-07-09 15:43:58 +0800

    sixth commit by zxh small-spring-step-10 基于JDK和Cglib动态代理，实现AOP核心功能 基于JDK的动态代理实现原理，public class JdkDynamicAopProxy implements AopProxy, InvocationHandler，在调用代理对象的方法时，会调用JdkDynamicAopProxy的invoke方法， 在invoke方法中，会判断当前方法是否是被代理的方法，如果是则执行methodInterceptor的invoke方法进行处理，这里会调用目标方法。

---
+++ small-spring-step-10/src/main/java/cn/bugstack/springframework/aop/Pointcut.java
@@ -0,0 +1,18 @@
+package cn.bugstack.springframework.aop;
+
+public interface Pointcut {
+
+    /**
+     * Return the ClassFilter for this pointcut.
+     * @return the ClassFilter (never <code>null</code>)
+     */
+    ClassFilter getClassFilter();
+
+    /**
+     * Return the MethodMatcher for this pointcut.
+     * @return the MethodMatcher (never <code>null</code>)
+     */
+    MethodMatcher getMethodMatcher();
+
+
+}
---
+++ small-spring-step-10/src/main/java/cn/bugstack/springframework/beans/factory/FactoryBean.java
@@ -0,0 +1,11 @@
+package cn.bugstack.springframework.beans.factory;
+
+public interface FactoryBean<T>{
+
+    T getObject() throws Exception;
+
+    Class<?> getObjectType();
+
+    boolean isSingleton();
+
+}
