6d2e98c45ef06e33ee3c43cca680de994475b3b6
zxh <zxh@163.com>
2022-07-15 19:30:35 +0800

    eleventh commit by zxh small-spring-step-15 三级缓存处理循环依赖 首先明确三级缓存是指singletonObjects：缓存某个 beanName 对应的经过了完整生命周期的bean； earlySingletonObjects：缓存提前拿原始对象进行了 AOP 之后得到的代理对象，原始对象还没有进行属性注入和后续的 BeanPostProcessor等生命周期 singletonFactories：缓存的是一个 ObjectFactory ，主要用来去生成原始对象进行了 AOP之后得到的「代理对象」，在每个 Bean 的生成过程中，都会提前暴露一个工厂，这个工厂可能用到，也可能用不到，如果没有出现循环依赖依赖本 bean，那么这个工厂无用，本 bean 按照自己的生命周期执行，执行完后直接把本 bean 放入 singletonObjects 中即可，如果出现了循环依赖依赖了本 bean，则另外那个 bean 执行 ObjectFactory 提交得到一个 AOP 之后的代理对象（如果有 AOP 的话，如果无需 AOP ，则直接得到一个原始对象）。 原理： (1)第一种情况：A和B之间发生循环依赖，但是没有AOP介入。 在A执行完实例化之后，此时会在三级缓存中放一个ObjectFactory，这是一个函数式接口，接口中的方法会返回创建的实例对象。接着执行A的属性注入操作，这时需要创建B，进入B的创建过程，也是实例化，在三级缓存中放ObjectFactory。接着B属性注入需要A，getBean时会从三级缓存中获取到ObjectFactory对象，调用getObject获取半成品A，此次调用会把A从三级缓存放到二级缓存中，此时B完成属性注入，B会放在一级缓存中。回到A的属性注入过程，当B完成创建以后，A的属性注入结束，将A也存放在一级缓存。这种情况ObjectFactory没啥作用。 (2)第二种情况：A和B之间发生循环依赖，A介入了AOP。 这种情况就体现了ObjectFactory的作用了。大致过程与上面一样，不一样的是，在A对应的ObjectFactory中getObject方法会调用 DefaultAdvisorAutoProxyCreator的getEarlyBeanReference方法，这时会生成A的代理对象，不过这是在调用getObject方法时会生成。在B实例化之后，属性注入时，会从三级缓存中获取A，这时调用了ObjectFactory中getObject方法，也就是获得了A的代理对象，但是这个A还没有完成属性注入，因此会从三级缓存转移到二级缓存，接着B完成属性注入，B创建完成，回到A的属性注入，A属性注入完成后，按道理会再次在initializeBean中执行AOP操作，但是DefaultAdvisorAutoProxyCreator缓存了已经执行AOP的对象，因此在这次后置处理中不会再次执行AOP，A也创建完成。

---
+++ small-spring-step-15/src/main/java/cn/bugstack/springframework/aop/ClassFilter.java
@@ -0,0 +1,13 @@
+package cn.bugstack.springframework.aop;
+
+public interface ClassFilter {
+
+    /**
+     * Should the pointcut apply to the given interface or target class?
+     * @param clazz the candidate target class
+     * @return whether the advice should apply to the given target class
+     */
+    boolean matches(Class<?> clazz);
+
+
+}
---
+++ small-spring-step-15/src/main/java/cn/bugstack/springframework/aop/framework/AopProxy.java
@@ -0,0 +1,7 @@
+package cn.bugstack.springframework.aop.framework;
+
+public interface AopProxy {
+
+    Object getProxy();
+
+}
